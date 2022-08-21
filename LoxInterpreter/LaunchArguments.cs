1f09033b3d2a43273a4c91650650d46ba67cc241
Richard Osborn <richardosborn256@gmail.com>
2022-08-20 22:45:00 -0700

    Add filepath arg

--- LoxInterpreter/LaunchArguments.cs
+++ LoxInterpreter/LaunchArguments.cs
@@ -5,5 +5,7 @@ namespace CommandLine
     [Serializable]
     internal class LaunchArguments
     {
+        [Option('i', nameof(FilePath), HelpText = "The source file to load.")]
+        public string FilePath { get; set; }
     }
 }
