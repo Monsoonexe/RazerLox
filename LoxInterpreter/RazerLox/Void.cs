8e17b18fc1a0dda9a144b2ca362db7407f108ab5
Richard Osborn <richardosborn256@gmail.com>
2022-08-22 23:57:50 -0700

    Create Void.cs

---
+++ LoxInterpreter/RazerLox/Void.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LoxInterpreter.RazerLox
+{
+    /// <summary>
+    /// Void represents nothing, very much like <see langword="void"/>.
+    /// Void exists as an objectified void because C# can't do IVisitor{void} like Java can.
+    /// </summary>
+    public struct Void : IEquatable<Void>
+    {
+        public static readonly Void Default = default(Void);
+
+        public static bool operator ==(Void left, Void right) => true;
+
+        public static bool operator !=(Void left, Void right) => false;
+
+        public override bool Equals(object obj)
+        {
+            return obj is Void;
+        }
+
+        public bool Equals(Void other)
+        {
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            return "()";
+        }
+    }
+}
