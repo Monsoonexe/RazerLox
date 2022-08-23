6404dd30f0cc0fee1d1bcea90d654148d08a0423
Richard Osborn <richardosborn256@gmail.com>
2022-08-23 00:18:58 -0700

    Create temp exitException

---
+++ LoxInterpreter/RazerLox/ExitException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LoxInterpreter.RazerLox
+{
+    internal class ExitException : Exception
+    {
+        public ExitException()
+            : base()
+        {
+
+        }
+
+        public ExitException(string message)
+            : base(message)
+        {
+
+        }
+    }
+}
