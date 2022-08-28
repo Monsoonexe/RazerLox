c04d93b630980544c016517b1b7a7460828014b9
Richard Osborn <richardosborn256@gmail.com>
2022-08-27 23:28:43 -0700

    Implement class constructors

--- LoxInterpreter/RazerLox/LoxClass.cs
+++ LoxInterpreter/RazerLox/LoxClass.cs
@@ -6,13 +6,21 @@ namespace LoxInterpreter.RazerLox
     /// <summary>
     /// Runtime representation of a class.
     /// </summary>
-    internal class LoxClass
+    internal class LoxClass : ILoxCallable
     {
         public readonly string identifier;
+
+        public int Arity => 0;
 
         public LoxClass(string identifier)
         {
             this.identifier = identifier;
+        }
+
+        public object Call(Interpreter interpreter, IList<object> arguments)
+        {
+            var instance = new LoxInstance(this);
+            return instance;
         }
 
         public override string ToString()
---
+++ LoxInterpreter/RazerLox/LoxInstance.cs
@@ -0,0 +1,17 @@
+﻿namespace LoxInterpreter.RazerLox
+{
+    internal class LoxInstance
+    {
+        private LoxClass loxClass;
+
+        public LoxInstance(LoxClass loxClass)
+        {
+            this.loxClass = loxClass;
+        }
+
+        public override string ToString()
+        {
+            return loxClass.identifier + " instance";
+        }
+    }
+}
