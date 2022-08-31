8768734a161c8cf619023f5c1056949dd388c509
Richard Osborn <richardosborn256@gmail.com>
2022-08-30 23:12:11 -0700

    Update EFunctionType.cs
    with Initializer and documentation.

--- LoxInterpreter/RazerLox/EFunctionType.cs
+++ LoxInterpreter/RazerLox/EFunctionType.cs
@@ -2,8 +2,24 @@
 {
     internal enum EFunctionType
     {
+        /// <summary>
+        /// Top-level statements.
+        /// </summary>
         None = 0,
+
+        /// <summary>
+        /// Some user-defined function.
+        /// </summary>
         Function = 1,
+
+        /// <summary>
+        /// Function that is a member of a class.
+        /// </summary>
         Method = 2,
+
+        /// <summary>
+        /// Specific 'constructor' method of a class.
+        /// </summary>
+        Initializer = 3,
     }
 }
