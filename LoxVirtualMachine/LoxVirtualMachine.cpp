c0d8205a764e5cbf4171c07d1bf84b2c61594516
Richard Osborn <richardosborn256@gmail.com>
2022-08-31 22:50:59 -0700

    Create LoxVirtualMachine cpp project

---
+++ LoxVirtualMachine/LoxVirtualMachine.cpp
@@ -0,0 +1,11 @@
+// LoxVirtualMachine.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <stdio.h>
+
+int main(int argc, const char* argv[])
+{
+    std::cout << "Hello World!\n";
+    return 0; // and so it begins!!!!
+}
